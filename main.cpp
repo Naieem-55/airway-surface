9c5aa67d54b3a20b4bcb35fbe23049675581fe69
muimr <muimr@IMRAN>
2025-01-09 13:47:05 +0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,400 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "CSE 4208: Computer Graphics Laboratory", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile our shader zprogram
+    Shader lightingShader("vertexShaderForPhongShading.vs", "fragmentShaderForPhongShading.fs");
+    //Shader lightingShader("vertexShaderForGouraudShading.vs", "fragmentShaderForGouraudShading.fs");
+    Shader ourShader("vertexShader.vs", "fragmentShader.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+
+    glm::vec4 domeAmbient = glm::vec4(1.0, 1.0, 0.8, 1.0);
+    glm::vec4 domeDiffusive = glm::vec4(1.0, 1.0, 0.8, 1.0);
+    glm::vec4 domeSpecular = glm::vec4(1.0, 1.0, 0.8, 1.0);
+    float domeShiny = 32.0f;
+
+    BezierCurve dome = BezierCurve(domeVerties, 75, domeAmbient, domeDiffusive, domeSpecular, domeShiny,0);
+    BezierCurve semiDome = BezierCurve(semiDomeVerties, 54, domeAmbient, domeDiffusive, domeSpecular, domeShiny,0);
+    BezierCurve minar = BezierCurve(minarVertices, 60, domeAmbient, domeDiffusive, domeSpecular, domeShiny,0);
+
+    glm::vec4 cylinderAmbient = glm::vec4(0.1, 0.9, 0.1, 1.0);
+    glm::vec4 cylinderDiffusive = glm::vec4(0.1, 0.9, 0.1, 1.0);
+    glm::vec4 cylinderSpecular = glm::vec4(0.0, 0.9, 0.0, 1.0);
+    float cylinderShiny = 12.0f;
+    BezierCurve greencylinder = BezierCurve(solinoidVertices, 66, cylinderAmbient, cylinderDiffusive, cylinderSpecular, cylinderShiny,0);
+
+    cylinderAmbient = glm::vec4(0.7, 0.3, 0.3, 1.0);
+    cylinderDiffusive = glm::vec4(0.7, 0.3, 0.3, 1.0);
+    cylinderSpecular = glm::vec4(0.7, 0.3, 0.3, 1.0);
+    BezierCurve greycylinder = BezierCurve(solinoidVertices, 66, cylinderAmbient, cylinderDiffusive, cylinderSpecular, cylinderShiny,0);
+    
+    glm::vec4 treeAmbient = glm::vec4(0.0, 0.9, 0.0, 1.0);
+    glm::vec4 treeDiffusive = glm::vec4(0.0, 0.9, 0.0, 1.0);
+    glm::vec4 treeSpecular = glm::vec4(0.0, 1.0, 0.0, 1.0);
+    float treeShiny = 12.0f;
+    BezierCurve tree = BezierCurve(treeVertices, 138, treeAmbient, treeDiffusive, treeSpecular, treeShiny, 0);
+
+
+    BezierCurve dome2 = BezierCurve(domeVerties, 75, domeAmbient, domeDiffusive, domeSpecular, domeShiny, 1);
+
+    glm::vec4 octAmbient = glm::vec4(0.5, 0.5, 0.5, 1.0);
+    glm::vec4 octDiffusive = glm::vec4(0.5, 0.5, 0.5, 1.0);
+    glm::vec4 octSpecular = glm::vec4(0.7, 0.7, 0.7, 1.0);
+    float octShiny = 32.0f;
+
+    glm::vec4 octWhite = glm::vec4(0.7, 0.7, 0.7, 1.0);
+    glm::vec4 octGrey = glm::vec4(0.7, 0.3, 0.3, 1.0);
+
+    Octagon oct1 = Octagon(octAmbient, octDiffusive, octSpecular, octShiny);
+    Octagon oct2 = Octagon(octWhite, octWhite, octWhite, octShiny);
+    Octagon oct3 = Octagon(octGrey, octGrey, octGrey, octShiny);
+
+
+    unsigned int cubeVAO, cubeVBO, cubeEBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glGenBuffers(1, &cubeEBO);
+
+    glBindVertexArray(cubeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // vertex normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)12);
+    glEnableVertexAttribArray(1);
+
+    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+    unsigned int lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glBindVertexArray(lightCubeVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
+    // note that we update the lamp's position attribute's stride to reflect the updated buffer data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    string diffuseMapPath = "rsz_11field_image.jpg";
+    string specularMapPath = "rsz_11field_image.jpg";
+
+
+    unsigned int diffMap = loadTexture(diffuseMapPath.c_str(), GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    unsigned int specMap = loadTexture(specularMapPath.c_str(), GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    Sphere sphere = Sphere(diffMap,specMap,0,0,2,1);
+
+    Cube texcube = Cube(diffMap, specMap, 32.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    Shader lightingShaderWithTexture("vertexShaderForPhongShadingWithTexture.vs", "fragmentShaderForPhongShadingWithTexture.fs");
+    //Shader ourShader("vertexShader.vs", "fragmentShader.fs");
+
+    diffuseMapPath = "rsz_1texture-grass-field.jpg";
+    specularMapPath = "rsz_1texture-grass-field.jpg";
+
+
+    diffMap = loadTexture(diffuseMapPath.c_str(), GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    specMap = loadTexture(specularMapPath.c_str(), GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    Cube cube = Cube(diffMap, specMap, 32.0f, 0.0f, 0.0f, 2.0f, 2.0f);
+    
+
+    diffuseMapPath = "sky.jpg";
+    specularMapPath = "sky.jpg";
+
+
+    diffMap = loadTexture(diffuseMapPath.c_str(), GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    specMap = loadTexture(specularMapPath.c_str(), GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+    Cube texcube2 = Cube(diffMap, specMap, 32.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+
+    
+
+    //ourShader.use();
+    //lightingShader.use();
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        lightingShader.use();
+        lightingShader.setVec3("viewPos", camera.Position);
+
+
+        pointlight1.setUpPointLight(lightingShader);
+        pointlight2.setUpPointLight(lightingShader);
+        pointlight3.setUpPointLight(lightingShader);
+        pointlight4.setUpPointLight(lightingShader);
+
+        spotlight.setUpSpotLight(lightingShader);
+
+        moonlight.setUpDirectionalLight(lightingShader);
+        daylight.setUpDirectionalLight(lightingShader);
+
+
+        // pass projection matrix to shader (note that in this case it could change every frame)
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 400.0f);
+        //glm::mat4 projection = glm::ortho(-2.0f, +2.0f, -1.5f, +1.5f, 0.1f, 100.0f);
+        lightingShader.setMat4("projection", projection);
+        
+
+        // camera/view transformation
+        glm::mat4 view = camera.GetViewMatrix();
+        //glm::mat4 view = basic_camera.createViewMatrix();
+        lightingShader.setMat4("view", view);
+
+
+        // Modelling Transformation
+        glm::mat4 identityMatrix = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 translate, rotate, revtranslate, alTogether, next, model, scale;
+        //translateMatrix = glm::translate(identityMatrix, glm::vec3(10, 20, 10));
+        //rotateXMatrix = glm::rotate(identityMatrix, glm::radians(rotateAngle_X), glm::vec3(1.0f, 0.0f, 0.0f));
+        //rotateYMatrix = glm::rotate(identityMatrix, glm::radians(rotateAngle_Y), glm::vec3(0.0f, 1.0f, 0.0f));
+        //rotateZMatrix = glm::rotate(identityMatrix, glm::radians(rotateAngle_Z), glm::vec3(0.0f, 0.0f, 1.0f));
+        //scaleMatrix = glm::scale(identityMatrix, glm::vec3(0.1, 0.1, 0.1));
+        model =  identityMatrix;
+        lightingShader.setMat4("model", model);
+
+
+        //scale = glm::scale(identityMatrix, glm::vec3(4.0, 4.0, 4.0));
+        //dome2.drawBezierCurve(lightingShader, scale);
+
+        model = identityMatrix;
+        //drawLake(cubeVAO, lightingShader, model);
+        drawField(cubeVAO, lightingShader, model);
+        drawFloor(cubeVAO, lightingShader, model);
+
+        rotate = glm::rotate(identityMatrix, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        translate = glm::translate(identityMatrix, glm::vec3(0.0, 0.0, 133.0));
+        model = translate * rotate;
+        drawField(cubeVAO, lightingShader, model);
+
+         
+
+        //Tajmahal design
+        translate = glm::translate(identityMatrix, glm::vec3(0.0, 2.0, -8.0));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0, 1.3, 1.0));
+        next = scale * translate;
+        //drawTajmahal(cubeVAO, lightingShader, next);
+        //central dome
+        translate = glm::translate(identityMatrix, glm::vec3(-3.5f, 12.0f, -24.5f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0, 1.0, 1.0));
+        model = next * translate * scale;
+        //drawDome(cubeVAO, dome, oct2, lightingShader, model);
+        //SDFL
+        translate = glm::translate(identityMatrix, glm::vec3(-10.0f, 12.0f, -16.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.5, 1.5, 1.5));
+        model = next * translate * scale;
+        //drawSemiDome(cubeVAO, semiDome, oct2, oct2, lightingShader, model);
+        //SDFR
+        translate = glm::translate(identityMatrix, glm::vec3(5.0f, 12.0f, -16.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.5, 1.5, 1.5));
+        model = next * translate * scale;
+        //drawSemiDome(cubeVAO, semiDome, oct2, oct2, lightingShader, model);
+        //SDBL
+        translate = glm::translate(identityMatrix, glm::vec3(-10.0f, 12.0f, -31.5f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.5, 1.5, 1.5));
+        model = next * translate * scale;
+        //drawSemiDome(cubeVAO, semiDome, oct2, oct2, lightingShader, model);
+        //SDBR
+        translate = glm::translate(identityMatrix, glm::vec3(5.0f, 12.0f, -31.5f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.5, 1.5, 1.5));
+        model = next * translate * scale;
+        //drawSemiDome(cubeVAO, semiDome, oct2, oct2, lightingShader, model);
+
+
+        //Minar right
+        translate = glm::translate(identityMatrix, glm::vec3(1.5, 0.0, -2.5));
+        model = next * translate;
+        drawMinar(cubeVAO, minar, semiDome, oct3, oct2, lightingShader, model);
+        //Minar left
+        translate = glm::translate(identityMatrix, glm::vec3(-22.5, 0.0, -2.5));
+        model = next * translate;
+        //drawMinar(cubeVAO, minar, semiDome, oct3, oct2, lightingShader, model);
+        //Minar right back
+        translate = glm::translate(identityMatrix, glm::vec3(17.5, 0.0, -42.5));
+        model = next * translate;
+        //drawMinar(cubeVAO, minar, semiDome, oct3, oct2, lightingShader, model);
+        //Minar left back
+        translate = glm::translate(identityMatrix, glm::vec3(-22.5, 0.0, -42.5));
+        model = next * translate;
+        //drawMinar(cubeVAO, minar, semiDome, oct3, oct2, lightingShader, model);
+
+        //drawNarrowMinarTogether(cubeVAO, minar, semiDome, oct3, oct2, lightingShader, next);
+        
+
+
+        model = identityMatrix;
+       
+
+        // we now draw as many light bulbs as we have point lights.
+        glBindVertexArray(lightCubeVAO);
+        for (unsigned int i = 0; i < 4; i++)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, pointLightPositions[i]);
+            model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+            ourShader.setMat4("model", model);
+            ourShader.setVec3("color", glm::vec3(0.8f, 0.8f, 0.8f));
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, spotLightPosition);
+        model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+        ourShader.setMat4("model", model);
+        ourShader.setVec3("color", glm::vec3(0.8f, 0.8f, 0.8f));
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+
+        lightingShaderWithTexture.use();
+        lightingShaderWithTexture.setVec3("viewPos", camera.Position);
+
+        lightingShaderWithTexture.setMat4("projection", projection);
+        lightingShaderWithTexture.setMat4("view", view);
+
+        lightingShaderWithTexture.use();
+
+        
+
+        pointlight1.setUpPointLight(lightingShaderWithTexture);
+        pointlight2.setUpPointLight(lightingShaderWithTexture);
+        pointlight3.setUpPointLight(lightingShaderWithTexture);
+        pointlight4.setUpPointLight(lightingShaderWithTexture);
+
+        spotlight.setUpSpotLight(lightingShaderWithTexture);
+
+        moonlight.setUpDirectionalLight(lightingShaderWithTexture);
+        daylight.setUpDirectionalLight(lightingShaderWithTexture);
+
+
+        
+        model = identityMatrix;
+        lightingShaderWithTexture.setMat4("model", model);
+        //drawFieldWithTexture(lightingShaderWithTexture, model);
+
+
+        translate = glm::translate(identityMatrix, glm::vec3(0.0f, 0.0f, 30.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(90.0f, 90.0f, 90.0f));
+        rotate = glm::rotate(identityMatrix, glm::radians(20.0f * 0.05f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = translate * scale;
+        //sphere.drawSphereWithTexture(lightingShaderWithTexture, model);;
+
+       
+
+
+
+
+        translate = glm::translate(identityMatrix, glm::vec3(-57.0f, -5.0f, -57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(114.0f, 114.0f, 1.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        translate = glm::translate(identityMatrix, glm::vec3(57.0f, -5.0f, -57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0f, 114.0f, 114.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+        
+        translate = glm::translate(identityMatrix, glm::vec3(-57.0f, -5.0f, -57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0f, 114.0f, 114.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+        
+        translate = glm::translate(identityMatrix, glm::vec3(57.0f, -5.0f, -57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0f, 114.0f, 114.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        translate = glm::translate(identityMatrix, glm::vec3(-57.0f, -5.0f, 57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0f, 114.0f, 114.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        translate = glm::translate(identityMatrix, glm::vec3(57.0f, -5.0f, 57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(1.0f, 114.0f, 114.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        translate = glm::translate(identityMatrix, glm::vec3(-57.0f, -5.0f, 138.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(114.0f, 114.0f, 1.0f));
+        model = translate * scale;
+        texcube.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        translate = glm::translate(identityMatrix, glm::vec3(-57.0f, 100.0f, -57.0f));
+        scale = glm::scale(identityMatrix, glm::vec3(190.0f, 1.0f, 200.0f));
+        model = translate * scale;
+        texcube2.drawCubeWithTexture(lightingShaderWithTexture, model);
+
+        glm::mat4 modelMatrixForContainer = glm::mat4(1.0f);
+        modelMatrixForContainer = glm::translate(identityMatrix, glm::vec3(0.0f, 3.0f, 2.0f));
+        //cube.drawCubeWithTexture(lightingShaderWithTexture, modelMatrixForContainer);
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &cubeVBO);
+    glDeleteBuffers(1, &cubeEBO);
+
+    glfwTerminate();
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,360 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.h"
+#include "camera.h"
+#include "basic_camera.h"
+#include "pointLight.h"
+#include "sphere.h"
+#include "spotLight.h"
+#include "directionLight.h"
+#include "cube.h"
+#include "stb_image.h"
+#include "curve.h"
+#include "sphere.h"
+#include "octagon.h"
+
+#include "shape_maker_functions.h"
+#include "vertex_values.h"
+
+// settings
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 1000;
+
+
+
+// camera
+Camera camera(glm::vec3(0.0f, 35.0f, 135.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+float eyeX = 0.0, eyeY = 1.0, eyeZ = 3.0;
+float lookAtX = 0.0, lookAtY = 0.0, lookAtZ = 0.0;
+glm::vec3 V = glm::vec3(0.0f, 1.0f, 0.0f);
+
+
+// positions of the point lights
+glm::vec3 pointLightPositions[] = {
+    glm::vec3(2.50f,  20.50f,  -28.0f),
+    glm::vec3(2.50f,  20.5f,  -18.0f),
+    glm::vec3(-2.5f,  20.5f,  -28.0f),
+    glm::vec3(-2.5f,  20.5f,  -18.0f)
+};
+
+glm::vec3 spotLightPosition = glm::vec3(0.0f, 20.5f, 68.0f);
+
+SpotLight spotlight(
+
+    spotLightPosition.x, spotLightPosition.y, spotLightPosition.z,  // position
+    0.0f, -1.0f, 0.0f,   //direction
+    0.0f, 0.5f, 0.5f,    // ambient
+    0.0f, 1.0f, 1.0f,    // diffuse
+    0.0f, 1.0f, 1.0f,    // specular
+    0.05f,   //k_c
+    0.009f,  //k_l
+    0.0032f, //k_q
+    60,     //light angle
+    5       // light number
+);
+
+DirectionLight moonlight(
+
+    0.0f, -1.0f, -0.5f,   //direction
+    0.031f, 0.04f, 0.045f,    // ambient
+    0.031f, 0.04f, 0.45f,    // diffuse
+    1.0f, 1.0f, 1.0f,    // specular
+    7       // light number
+);
+
+DirectionLight daylight(
+
+    0.0f, -1.0f, -0.5f,   //direction
+    0.4f, 0.4f, 0.4f,    // ambient
+    0.8f, 0.8f, 0.8f,    // diffuse
+    1.0f, 1.0f, 1.0f,    // specular
+    6       // light number
+);
+
+PointLight pointlight1(
+
+    pointLightPositions[0].x, pointLightPositions[0].y, pointLightPositions[0].z,  // position
+    0.1f, 0.05f, 0.1f,     // ambient
+    0.8f, 0.8f, 0.8f,     // diffuse
+    1.0f, 1.0f, 1.0f,        // specular
+    1.0f,   //k_c
+    0.09f,  //k_l
+    0.032f, //k_q
+    1       // light number
+);
+PointLight pointlight2(
+
+    pointLightPositions[1].x, pointLightPositions[1].y, pointLightPositions[1].z,  // position
+    0.05f, 0.05f, 0.15f,     // ambient
+    0.8f, 0.8f, 0.8f,     // diffuse
+    1.0f, 1.0f, 1.0f,        // specular
+    1.0f,   //k_c
+    0.09f,  //k_l
+    0.032f, //k_q
+    2       // light number
+);
+PointLight pointlight3(
+
+    pointLightPositions[2].x, pointLightPositions[2].y, pointLightPositions[2].z,  // position
+    0.05f, 0.1f, 0.1f,     // ambient
+    0.8f, 0.8f, 0.8f,     // diffuse
+    1.0f, 1.0f, 1.0f,        // specular
+    1.0f,   //k_c
+    0.09f,  //k_l
+    0.032f, //k_q
+    3       // light number
+);
+PointLight pointlight4(
+
+    pointLightPositions[3].x, pointLightPositions[3].y, pointLightPositions[3].z,  // position
+    0.1f, 0.1f, 0.05f,     // ambient
+    0.8f, 0.8f, 0.8f,     // diffuse
+    1.0f, 1.0f, 1.0f,        // specular
+    1.0f,   //k_c
+    0.09f,  //k_l
+    0.032f, //k_q
+    4       // light number
+);
+
+
+// light settings
+bool pointLightOn = true;
+bool spotLightOn = true;
+bool dayLightOn = true;
+bool moonLightOn = true;
+bool ambientToggle = true;
+bool diffuseToggle = true;
+bool specularToggle = true;
+
+
+// timing
+float deltaTime = 0.0f;    // time between current frame and last frame
+float lastFrame = 0.0f;
+
+
+
+
+
+/*
+    Process Input
+*/
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    std::cout << "scrolling " << std::endl;
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
+        camera.ProcessKeyboard(YAWR, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
+        camera.ProcessKeyboard(YAWL, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+        camera.ProcessKeyboard(PITCHU, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
+        camera.ProcessKeyboard(PITCHD, deltaTime);
+    }
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_1 && action == GLFW_PRESS)
+    {
+        if (pointLightOn)
+        {
+            pointlight1.turnOff();
+            pointlight2.turnOff();
+            pointlight3.turnOff();
+            pointlight4.turnOff();
+            pointLightOn = !pointLightOn;
+        }
+        else
+        {
+            pointlight1.turnOn();
+            pointlight2.turnOn();
+            pointlight3.turnOn();
+            pointlight4.turnOn();
+            pointLightOn = !pointLightOn;
+        }
+    }
+
+
+    else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+    {
+        if (specularToggle)
+        {
+
+            pointlight1.turnSpecularOff();
+            pointlight2.turnSpecularOff();
+            pointlight3.turnSpecularOff();
+            pointlight4.turnSpecularOff();
+
+            specularToggle = !specularToggle;
+        }
+        else
+        {
+
+            pointlight1.turnSpecularOn();
+            pointlight2.turnSpecularOn();
+            pointlight3.turnSpecularOn();
+            pointlight4.turnSpecularOn();
+            specularToggle = !specularToggle;
+        }
+    }
+
+    else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+    {
+        if (diffuseToggle)
+        {
+
+            pointlight1.turnDiffuseOff();
+            pointlight2.turnDiffuseOff();
+            pointlight3.turnDiffuseOff();
+            pointlight4.turnDiffuseOff();
+            diffuseToggle = !diffuseToggle;
+        }
+        else
+        {
+
+            pointlight1.turnDiffuseOn();
+            pointlight2.turnDiffuseOn();
+            pointlight3.turnDiffuseOn();
+            pointlight4.turnDiffuseOn();
+            diffuseToggle = !diffuseToggle;
+        }
+    }
+
+    else if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS)
+    {
+        if (ambientToggle)
+        {
+
+            pointlight1.turnAmbientOff();
+            pointlight2.turnAmbientOff();
+            pointlight3.turnAmbientOff();
+            pointlight4.turnAmbientOff();
+            ambientToggle = !ambientToggle;
+        }
+        else
+        {
+
+            pointlight1.turnAmbientOn();
+            pointlight2.turnAmbientOn();
+            pointlight3.turnAmbientOn();
+            pointlight4.turnAmbientOn();
+            ambientToggle = !ambientToggle;
+        }
+    }
+
+    else if (glfwGetKey(window, GLFW_KEY_5) == GLFW_PRESS)
+    {
+        if (spotLightOn)
+        {
+
+            spotlight.turnOff();
+            spotLightOn = !spotLightOn;
+        }
+        else
+        {
+            spotlight.turnOn();
+            spotLightOn = !spotLightOn;
+        }
+    }
+
+    else if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+    {
+        if (dayLightOn)
+        {
+
+            daylight.turnOff();
+            dayLightOn = false;
+        }
+        else
+        {
+            daylight.turnOn();
+            dayLightOn = true;
+        }
+    }
+
+    else if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
+    {
+        if (moonLightOn)
+        {
+
+            moonlight.turnOff();
+            moonLightOn = !moonLightOn;
+        }
+        else
+        {
+            moonlight.turnOn();
+            moonLightOn = !moonLightOn;
+        }
+    }
+}
